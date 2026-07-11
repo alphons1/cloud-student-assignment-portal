f842089a20974fb85e360f00f32fd168238fd237
Alphons1 <currentone1234@gmai.com>
2026-07-11 22:47:00 +0800

    Improve dashboard metrics and UI

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -54,7 +54,7 @@ Built with:
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.metric("Files Stored", "1")
+    st.metric("Files Stored", len(df))
 
 with col2:
     st.metric("Cloud Provider", "AWS")
